3aa77d41e8248d4f4c8aca2e8723880fc280f531
SalahMSwefy <salahm.swefy@gmali.com>
2025-12-21 01:01:54 +0200

    edit config file name

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,49 +1,49 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-process.on('uncaughtException', (err) => {
-  // listting to event
-  console.log('UNCAUGHT EXCEPTION! Shutting down...');
-  console.log(err.name, err.message);
-  process.exit(1);
+process.on("uncaughtException", err => {
+    // listting to event
+    console.log("UNCAUGHT EXCEPTION! Shutting down...");
+    console.log(err.name, err.message);
+    process.exit(1);
 });
 
-dotenv.config({ path: './config.env' }); //requring the config.env file
-const app = require('./app');
+dotenv.config({ path: "./.env" }); //requring the config.env file
+const app = require("./app");
 
 // console.log(process.env);
 
 // database connection
 
 const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD,
+    "<PASSWORD>",
+    process.env.DATABASE_PASSWORD
 );
 
 mongoose
-  .connect(DB, {
-    // return a promise
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('DB connetion successful!');
-  });
+    .connect(DB, {
+        // return a promise
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("DB connetion successful!");
+    });
 
 // server
 
 const portnumber = process.env.PORT || 3000;
 const server = app.listen(portnumber, () => {
-  console.log(`listening on port ${portnumber}`);
+    console.log(`listening on port ${portnumber}`);
 });
 
-process.on('unhandledRejection', (err) => {
-  // listting to event
-  console.log(err.name, err.message);
-  console.log('UNHANDLED REJECTION! Shutting down...');
-  server.close(() => {
-    process.exit(1);
-  });
+process.on("unhandledRejection", err => {
+    // listting to event
+    console.log(err.name, err.message);
+    console.log("UNHANDLED REJECTION! Shutting down...");
+    server.close(() => {
+        process.exit(1);
+    });
 });
